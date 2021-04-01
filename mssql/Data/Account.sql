0785bbb93c539942287c7ef0c4789300dfe5440a
Robin Huiser <robin@rdc.pt>
2021-04-01 12:21:46 +0100

    First test data, incl. automation

---
+++ mssql/Data/Account.sql
@@ -0,0 +1,5 @@
+--
+-- INSERT template for table `Account`
+--
+INSERT INTO `Account`(`id`, `type`, `number`, `parentId`, `entities`, `name`, `title`, `iban`, `routingnumbers`, `balances`, `dateCreated`, `dateOpened`, `dateLastUpdated`, `dateClosed`, `currencyCode`, `status`, `source`, `interestReporting`, `creditInformation`, `loanInterest`, `creditInterest`, `preferences`, `bank`, `owners`, `product`, `URI`) 
+VALUES ("123456-DDA", "DDA", "xxxxxxx1213", "12345ABC", null, "Matt Dollar", "401k Plan", "GB29NWBK60161331926819", null, null, "YYYY-MM-DDThh:mm:ss", "YYYY-MM-DDThh:mm:ss", "YYYY-MM-DDThh:mm:ss", "YYYY-MM-DDThh:mm:ss", "USD", "OPEN", "core", 1, null, null, null, null, null, null, null, "http://host/images/1234.jpg");
